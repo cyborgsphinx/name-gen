75c6b28f199e292393c223ad01b52670b23040f3
James Hannah <jhannah8124@gmail.com>
2018-01-15 16:52:08 -0800

    turns out this is easier in lua

---
+++ name-gen.lua
@@ -0,0 +1,20 @@
+phonemes = { 'ay', 'a', 'b', 'k', 'd', 'ee', 'ea',
+             'f', 'g', 'h', 'igh', 'i', 'j', 'l',
+             'm', 'n', 'oa', 'o', 'p', 'qu', 'r',
+             's', 't', 'ew', 'u', 'v', 'w', 'x',
+             'y', 'z', 'oo', 'oi', 'ou', 'aw', 'ar',
+             'sh', 'wh', 'ch', 'th', 'ng', 'er' }
+ 
+math.randomseed(os.time())
+num = 3
+if arg[1] ~= nil then
+    n = tonumber(arg[1])
+    num = (n > 3) and n or 3
+end
+out = ''
+for i=1,num do
+    val = math.random(table.getn(phonemes))
+    out = out .. phonemes[val]
+end
+
+io.write(out, '\n')
