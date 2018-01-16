ede1e607e4bdcfb1d562738ea64abca30b607d50
James Hannah <jhannah8124@gmail.com>
2018-01-15 16:51:07 -0800

    unified formatting of phonemes.cpp and removed temporary variable

--- src/phonemes.cpp
+++ src/phonemes.cpp
@@ -2,74 +2,54 @@
 
 using std::string;
 
-#define ADD(c) out += c; break
-
 namespace phonemes {
     string get_part(unsigned val) {
         unsigned num_phonemes = 30 + 12 + 1;
-        string out;
         switch (val % num_phonemes) {
-            case A:
-                ADD("ay");
-                //out += "ay"; break;
-            case a:
-                ADD('a');
-            case b:
-                ADD('b');
-            case k:
-                ADD('k');
-            case d:
-                ADD('d');
-            case E:
-                ADD("ee");
-            case e:
-                ADD("ea");
-            case f:
-                ADD('f');
-            case g:
-                ADD('g');
-            case h:
-                ADD('h');
-            case I:
-                ADD("igh");
-            case i:
-                ADD('i');
-            case j:
-                ADD('j');
-            case l:
-                ADD('l');
-            case m: ADD('m');
-            case n: ADD('n');
-            case O: ADD("oa");
-            case o: ADD('o');
-            case p:
-                    ADD('p');
-            case kw: ADD("qu");
-            case r: ADD('r');
+            case A: return "ay";
+            case a: return "a";
+            case b: return "b";
+            case k: return "k";
+            case d: return "d";
+            case E: return "ee";
+            case e: return "ea";
+            case f: return "f";
+            case g: return "g";
+            case h: return "h";
+            case I: return "igh";
+            case i: return "i";
+            case j: return "j";
+            case l: return "l";
+            case m: return "m";
+            case n: return "n";
+            case O: return "oa";
+            case o: return "o";
+            case p: return "p";
+            case kw: return "qu";
+            case r: return "r";
             case zh:
-            case s: ADD('s');
-            case t: ADD('t');
-            case U: ADD("ew");
-            case u: ADD('u');
-            case v: ADD('v');
-            case w: ADD('w');
-            case ks: ADD('x');
-            case y: ADD('y');
-            case z: ADD('z');
+            case s: return "s";
+            case t: return "t";
+            case U: return "ew";
+            case u: return "u";
+            case v: return "v";
+            case w: return "w";
+            case ks: return "x";
+            case y: return "y";
+            case z: return "z";
             case OO:
-            case oo: ADD("oo");
-            case oi: ADD("oi");
-            case ou: ADD("ou");
-            case aw: ADD("aw");
-            case ar: ADD("ar");
-            case sh: ADD("sh");
-            case hw: ADD("wh");
-            case ch: ADD("ch");
-            case th: ADD("th");
-            case ng: ADD("ng");
-            case er: ADD("er");
-            default: ADD('e');
+            case oo: return "oo";
+            case oi: return "oi";
+            case ou: return "ou";
+            case aw: return "aw";
+            case ar: return "ar";
+            case sh: return "sh";
+            case hw: return "wh";
+            case ch: return "ch";
+            case th: return "th";
+            case ng: return "ng";
+            case er: return "er";
+            default: return "e";
         }
-        return out;
     }
 }
